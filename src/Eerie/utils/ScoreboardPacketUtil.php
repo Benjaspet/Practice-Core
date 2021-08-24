972d1d09412e65435ae2ae47a1c84f50165df1c6
EerieAlchemist <66958528+EerieAlchemist@users.noreply.github.com>
2021-08-24 09:22:34 -0400

    added docs & scoreboard packet creator

--- src/Eerie/utils/ScoreboardPacketUtil.php
+++ src/Eerie/utils/ScoreboardPacketUtil.php
@@ -2,10 +2,18 @@
 
 namespace Eerie\utils;
 
+use pocketmine\network\mcpe\protocol\RemoveObjectivePacket;
 use pocketmine\network\mcpe\protocol\SetDisplayObjectivePacket;
+use pocketmine\network\mcpe\protocol\SetScorePacket;
+use pocketmine\network\mcpe\protocol\types\ScorePacketEntry;
 use pocketmine\Player;
 
 class ScoreboardPacketUtil {
+
+    /*
+     * @param Player $player - The player whose scoreboard title will be set.
+     * @param string $title - The scoreboard title display name.
+     */
 
     public function setScoreboardTitle(Player $player, string $title): void {
         $packet = new SetDisplayObjectivePacket();
@@ -14,6 +22,35 @@ class ScoreboardPacketUtil {
         $packet->displayName = $title;
         $packet->criteriaName = "dummy";
         $packet->sortOrder = 0;
+        $player->sendDataPacket($packet);
+    }
+
+    /*
+     * @param Player $player - The player whose scoreboard will be removed.
+     */
+
+    public function removeScoreboard(Player $player): void {
+        $packet = new RemoveObjectivePacket();
+        $packet->objectiveName = "objective";
+        $player->sendDataPacket($packet);
+    }
+
+    /*
+     * @param Player $player - The player whose scoreboard line will be updated.
+     * @param int $line - The line number.
+     * @param string $content - The line content.
+     */
+
+    public function createScoreboardLine(Player $player, int $line, string $content): void {
+        $packetline = new ScorePacketEntry();
+        $packetline->objectiveName = "objective";
+        $packetline->type = ScorePacketEntry::TYPE_FAKE_PLAYER;
+        $packetline->customName = " ". $content . "   ";
+        $packetline->score = $line;
+        $packetline->scoreboardId = $line;
+        $packet = new SetScorePacket();
+        $packet->type = SetScorePacket::TYPE_CHANGE;
+        $packet->entries[] = $packetline;
         $player->sendDataPacket($packet);
     }
 
